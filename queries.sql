cbe8afaab550da87b7a079b47692fe57f7d66763
otmanTR <r.brk.otman@gmail.com>
2023-04-05 13:39:55 +0300

    insert pokemons

--- queries.sql
+++ queries.sql
@@ -7,3 +7,4 @@ SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered = true;
 SELECT * FROM animals WHERE name != 'Gabumon';
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+
