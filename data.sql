b74b7e01fb928362e1683c797f32e796839d5829
otmanTR <r.brk.otman@gmail.com>
2023-04-05 21:38:02 +0300

    modify animals with owner_id

--- data.sql
+++ data.sql
@@ -55,3 +55,16 @@ UPDATE animals
  END);
 
 SELECT id, name, species_id FROM animals;
+
+--modify animals owners
+
+UPDATE animals
+SET owner_id = (CASE
+ WHEN name IN ('Agumon') THEN (SELECT id FROM owners WHERE full_name = 'Sam Smith')
+ WHEN name IN ('Gabumon', 'Pikachu') THEN (SELECT id FROM owners WHERE full_name = 'Jennifer Orwell')
+ WHEN name IN ('Devimon', 'Plantmon') THEN (SELECT id FROM owners WHERE full_name = 'Bob')
+ WHEN name IN ('Charmander', 'Squirtle', 'Blossom') THEN (SELECT id FROM owners WHERE full_name = 'Melody Pond')
+ WHEN name IN ('Angemon', 'Boarmon') THEN (SELECT id FROM owners WHERE full_name = 'Dean Winchester')
+ END);
+
+SELECT id, name, species_id, owner_id FROM animals;
