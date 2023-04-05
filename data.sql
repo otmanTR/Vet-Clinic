cb1f47676dc238aad14478e93bb2f2fa8790c5a8
otmanTR <r.brk.otman@gmail.com>
2023-04-05 21:21:55 +0300

    insert owners data

--- data.sql
+++ data.sql
@@ -24,3 +24,16 @@ VALUES ('Blossom', '1998-10-13', 3, 't', 17.0, '');
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg, species)
 VALUES ('Ditto', '2022-05-14', 4, 't', 22.0, '');
 
+/* Day three the used QUERIES */
+
+-- insert woners data 
+
+INSERT INTO owners (full_name, age) VALUES
+  ('Sam Smith', 34),
+  ('Jennifer Orwell', 19),
+  ('Bob', 45),
+  ('Melody Pond', 77),
+  ('Dean Winchester', 14),
+  ('Jodie Whittaker', 38);
+
+  SELECT * FROM owners;
