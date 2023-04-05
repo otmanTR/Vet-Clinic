92733f222c261ab4a2edc4e9e361c1d25c363757
otmanTR <r.brk.otman@gmail.com>
2023-04-05 23:52:12 +0300

    change requires

--- data.sql
+++ data.sql
@@ -9,20 +9,20 @@ VALUES
 
 /* Day two the used QUERIES */
 
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg, species)
-VALUES ('Charmander', '2020-02-08', 0, 'f', -11.0, '');
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg, species)
-VALUES ('Plantmon', '2021-11-15', 2, 't', -5.7, '');
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg, species)
-VALUES ('Squirtle', '1993-04-02', 3, 'f', -12.13, '');
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg, species)
-VALUES ('Angemon', '2005-06-12', 1, 't', -45.0, '');
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg, species)
-VALUES ('Boarmon', '2005-06-07', 7, 't', 20.4, '');
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg, species)
-VALUES ('Blossom', '1998-10-13', 3, 't', 17.0, '');
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg, species)
-VALUES ('Ditto', '2022-05-14', 4, 't', 22.0, '');
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES ('Charmander', '2020-02-08', 0, 'f', -11.0);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES ('Plantmon', '2021-11-15', 2, 't', -5.7);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES ('Squirtle', '1993-04-02', 3, 'f', -12.13);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES ('Angemon', '2005-06-12', 1, 't', -45.0);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES ('Boarmon', '2005-06-07', 7, 't', 20.4);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES ('Blossom', '1998-10-13', 3, 't', 17.0);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES ('Ditto', '2022-05-14', 4, 't', 22.0);
 
 /* Day three the used QUERIES */
 
--- schema.sql
+++ schema.sql
@@ -16,18 +16,18 @@ ALTER TABLE animals ADD COLUMN species VARCHAR(50);
 
 -- add owners table
 
-CREATE TABLE owners ( id SERIAL PRIMARY KEY, full_name VARCHAR(255), age INTEGER )
+CREATE TABLE owners ( id SERIAL PRIMARY KEY, full_name VARCHAR(255), age INTEGER );
 
 --add species table
 
-CREATE TABLE species ( id SERIAL PRIMARY KEY, name VARCHAR(255) )
+CREATE TABLE species ( id SERIAL PRIMARY KEY, name VARCHAR(255) );
 
 --modify animals table
 
 ALTER TABLE animals
   DROP COLUMN species,
-  ADD COLUMN species_id INTEGER REFERENCES species(id)
-  ADD COLUMN owner_id INTEGER REFERENCES owners(id)
+  ADD COLUMN species_id INTEGER REFERENCES species(id),
+  ADD COLUMN owner_id INTEGER REFERENCES owners(id);
 
   ALTER TABLE animals
-  ALTER COLUMN id SET DEFAULT nextval('animals_id_seq')
+  ALTER COLUMN id SET DEFAULT nextval('animals_id_seq');
