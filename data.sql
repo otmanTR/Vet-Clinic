24826e2af7104e547d69f36c2a57aee5466e3fa4
otmanTR <r.brk.otman@gmail.com>
2023-04-06 13:32:05 +0300

    insert vets data

--- data.sql
+++ data.sql
@@ -68,3 +68,14 @@ SET owner_id = (CASE
  END);
 
 SELECT id, name, species_id, owner_id FROM animals;
+
+--insert data for vets
+
+  INSERT INTO vets (name, age, date_of_graduation)
+  VALUES
+  ( 'William Tatcher', 45, '2000-04-23'),
+  ('Maisy Smith', 26, '2019-01-17'),
+  ('Stephanie Mendez', 64, '1981-05-04'),
+  ('Jack Harkness', 38, '2008-06-08');
+
+
